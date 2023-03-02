f612ebb2a23983650a6ffacce0408bee79f13854
André Dantas <andre.americo@camara.leg.br>
2023-03-02 14:18:37 -0300

    Projeto django

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, formulario
 
 urlpatterns = [
     path('', index),
+    path('formulario/',formulario, name='formulario' ),
 ]
 
--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galeria/index.html')
+
+def formulario(request):
+    return render(request, 'galeria/formulario.html')
